544497fcfcc992ec59ac8fcb348d4990b6def81a
xashes <kellerx0319@gmail.com>
2020-05-05 16:11:13 +0800

    tortoise racing

---
+++ tortoise_racing.rkt
@@ -0,0 +1,32 @@
+#lang racket
+
+(module+ test
+  (require rackunit))
+
+;; Two tortoises named A and B must run a race. A starts with an average speed of 720 feet per hour. Young B knows she runs faster than A, and furthermore has not finished her cabbage.
+
+;; When she starts, at last, she can see that A has a 70 feet lead but B's speed is 850 feet per hour. How long will it take B to catch A?
+
+;; More generally: given two speeds v1 (A's speed, integer > 0) and v2 (B's speed, integer > 0) and a lead g (integer > 0) how long will it take B to catch A?
+
+;; The result will be an array [hour, min, sec] which is the time needed in hours, minutes and seconds (round down to the nearest second) or a string in some languages.
+
+;; If v1 >= v2 then return nil, nothing, null, None or {-1, -1, -1} for C++, C, Go, Nim, [] for Kotlin or "-1 -1 -1".
+
+(define/contract (race v1 v2 g)
+  (->  natural? natural? natural? list?)
+  (if (>= v1 v2)
+      null
+      (let ((sec (floor (* 3600 (/ g
+                                   (- v2 v1))))))
+        (let*-values (((hour m) (quotient/remainder sec 3600))
+                      ((m s) (quotient/remainder m 60)))
+          (list hour m s))))
+)
+
+(module+ test
+  (check-equal? (race 720 850 70)
+                '(0 32 18))
+  (check-equal? (race 80 91 37)
+                '(3 21 49))
+  )
